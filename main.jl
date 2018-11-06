868a34ba4eb0c201706f1318c3618d26a330011b
Jaqueline Ortiz <43250276+jaquelineortiz@users.noreply.github.com>
2018-11-05 23:29:56 -0200

    Update main.jl

--- main.jl
+++ main.jl
@@ -3,13 +3,13 @@ gr(size=(600,400))
 default(fmt = :png)
 
 function main()
-    D = readcsv("dados.csv") # Ler dados.csv
+    D = readcsv("dados.csv") 
     x = D[:,1]
     y = D[:,2]
 
     kfold(x, y)
 
-    p = 1 ####### Sua escolha
+    p = 8 
     xlin = linspace(extrema(x)..., 100)
     β = regressao_polinomial(x, y, p)
     ylin = β[1] * ones(100)
@@ -20,12 +20,12 @@ function main()
     plot!(xlin, ylin, c=:red, lw=2)
     png("ajuste")
     
-    y_pred = β[1]
-    for j = 1:p
-        y_pred .+= β[j+1] * x.^j
-    end
-    y_med = mean(y)
-    R2 = 1 - norm(y_pred - y)^2 / norm(y_med - y)^2  # Calcule a medida R²
+    #y_pred = zeros()
+    #for j = 1:p
+        #y_pred .+= β[j+1] * x.^j
+    #end
+    #y_med = mean(y)
+    #R2 = 1 - norm(y_pred - y)^2 / norm(y_med - y)^2  
 end
 
 function regressao_polinomial(x, y, p)
@@ -49,11 +49,17 @@ function kfold(x, y; num_folds = 5, max_p=15)
         for p = 1:max_p
             β = regressao_polinomial(x_tr, y_tr, p)
             y_pred = β[1] + sum(β[j+1]*x.^j for j=1:p)
-            erro_treino = (1/(2*lenght(cjto_treino))) * sum(y[i]-y_pred[i] for i=cjto_treino)^2
-            erro_teste = (1/(2*lenght(cjto_teste))) * sum(y[i]-y_pred[i] for i=cjto_teste)^2
+            erro_treino = (1/(2 * lenght(cjto_treino))) * sum(y[i]-y_pred[i] for i=cjto_treino)^2
+            erro_teste = (1/(2 * lenght(cjto_teste))) * sum(y[i]-y_pred[i] for i=cjto_teste)^2
             E_treino[fold,p] = erro_treino
             E_teste[fold,p] = erro_teste
         end
+        #Em_teste = mean(E_teste[fold,:])
+        #Em_treino = mean(E_treino[fold,:])
+        plot!(1:max_p, E_treino[fold,:], c=:blue, ms=1, leg=false)
+        plot!(1:max_p, E_teste[fold,:], c=:green, ms=1, leg=false)
+        #plot!(1:max_p, Em_treino[fold,:], c=:blue, ms=3, leg=false)
+        #plot!(1:max_p, Em_teste[fold,:], c=:green, ms=3, leg=false)
     end
     png("kfold")
 end
